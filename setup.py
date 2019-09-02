a8fae603a68042b9e7676c222a1b328eaad042bc
Kyle <kelmore5@gmail.com>
2019-09-02 15:40:21 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='kelmore-json',
       version='1.0',
-      description='Utility package for Python',
+      description='JSON utility package for Python',
       author='kelmore5',
       author_email='kelmore5@gmail.com',
       license='MIT',
